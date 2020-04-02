5b3e7daab1a1ed401533012a9f8ac43f8d96af3d
Anna <anyka69@mail.ru>
2020-04-02 21:05:24 +0300

    added

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,10 +1,12 @@
 package tests;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import pages.IntegriVideoChatPage;
 import pages.IntegriVideoSingUp;
+import untils.CapabilitiesGenerator;
 
 import java.util.concurrent.TimeUnit;
 
@@ -14,8 +16,10 @@ public class BaseTest {
     IntegriVideoSingUp singUp;
     @BeforeMethod
     public void setUp() {
+
 //        System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver.exe");
 //        driver = new ChromeDriver();
+        driver = new ChromeDriver(CapabilitiesGenerator.getChromeOptions());
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         driver.manage().window().maximize();
         chatPage = new IntegriVideoChatPage(driver);
